2954744ca473d09d535881f1193f3f937e060b32
roee <roy@mbtechnologyil.com>
2016-05-31 16:32:34 +0300

    test

--- streamer/index.js
+++ streamer/index.js
@@ -1,6 +1,6 @@
 /*
-* Assets Streamer
-*/
+ * Assets Streamer
+ */
 var ioClient = require('socket.io-client'),
     socket = ioClient.connect('http://sst-n2-c-nl.spotoption.com'),
     mysql = require('mysql'),
@@ -17,25 +17,7 @@ server.listen(8080);
 var AssetFeed = require('./assets-feed');
 var assetFeed = new AssetFeed();
 
-io.on('connection', function (socket) {
-
-    var feeds = [];
-    socket.on('add-feed', function (newFeeds) {
-        newFeeds = JSON.parse(newFeeds);
-        feeds = feeds.concat(newFeeds);
-    });
-
-    socket.on('feed-all', function() {
-        console.log('FEEDING ALL');
-        assetFeed.subscribe(function(asset, data, rate) {
-            if (asset == 91) {
-                console.log("Feeding");
-                console.log(feeds.indexOf(parseInt(asset)));
-            }
-            if (feeds.indexOf(parseInt(asset)) !== -1) {
-                socket.emit('feed_' + asset, JSON.stringify(data), rate);
-            }
-        });
-    });
+assetFeed.subscribe(function(asset, data, rate) {
+    io.sockets.emit('feed_' + asset, JSON.stringify(data), rate);
 });
 
